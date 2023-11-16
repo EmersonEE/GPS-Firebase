c3f445ee027160f88c47c14a84d72ebaead7e5e6
EmersonEE <perezemerson85@gmail.com>
2023-11-16 17:15:50 -0600

    Conectando a la Base de Datos

---
+++ src/credentials.h
@@ -0,0 +1,9 @@
+// Insert your network credentials
+#define WIFI_SSID "CLARO_h9hU3j"
+#define WIFI_PASSWORD "7474FB19FD"
+
+// Insert Firebase project API Key
+#define API_KEY "AIzaSyDPjKar_c5fyLUmYpTwUCi5Sp1MJKGxBrg"
+
+// Insert RTDB URLefine the RTDB URL */
+#define DATABASE_URL "https://gps-telegram-default-rtdb.firebaseio.com/" 
